8e3d3cf8b91d31729d5917a41c39f105ae1d7367
erl67 <erl67@pitt.edu>
2018-09-10 13:51:36 -0400

    init

---
+++ src/MasterMind.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : MasterMind.c
+ Author      : ERL67
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ int main() {
+	 printf("Hello World!\n");
+	 puts("https://github.com/erl67/MasterMind"); /* prints https://github.com/erl67/MasterMind */
+     return 0;
+ }

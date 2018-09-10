6a7ce62d06ca1f02f46b0e0347cc08d9288258aa
erl67 <erl67@pitt.edu>
2018-09-10 17:19:59 -0400

    nope

--- src/MasterMind.c
+++ src/MasterMind.c
@@ -2,17 +2,42 @@
  ============================================================================
  Name        : MasterMind.c
  Author      : ERL67
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+int colors;
+char str_input[6];
+
+
+
  int main() {
-	 printf("Hello World!\n");
-	 puts("https://github.com/erl67/MasterMind"); /* prints https://github.com/erl67/MasterMind */
+	 int input = 0;
+	 puts("\nMasterMind\n\n");
+
+	 do {
+
+		 printf("Enter the number of colors[3-6]: ");
+
+		 fgets(str_input, 3, stdin);
+
+//		 input = str_input[0];
+		 input = atoi(str_input);
+
+		 printf("\nYou entered s: %s", str_input);
+		 printf("\nYou entered u: %u", input);
+		 printf("\nYou entered i: %i\n\n", input);
+
+	 } while (input > 6 || input < 3 );
+
+
+
+
+
+
+
+
      return 0;
  }
